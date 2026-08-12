7d4b7e723ecb59b2d97e166953f52ddd3dfb2214
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-12 09:35:50 +0545

    Replace single-post placeholder

--- single.php
+++ single.php
@@ -1,5 +1,33 @@
-<?php get_header();?>
+<?php
+/**
+ * Default single-post template for the Best Design theme.
+ * Renders the current post title, featured image when available, and body copy.
+ */
 
-test DOCUMENT
+get_header();
+?>
 
-<?php get_footer();?>
+<main class="container pt-4 pb-4">
+    <?php
+    while ( have_posts() ) :
+        the_post();
+        ?>
+        <article <?php post_class(); ?>>
+            <h1><?php the_title(); ?></h1>
+
+            <?php if ( has_post_thumbnail() ) : ?>
+                <div class="single-featured-image">
+                    <?php the_post_thumbnail( 'full' ); ?>
+                </div>
+            <?php endif; ?>
+
+            <div class="single-content">
+                <?php the_content(); ?>
+            </div>
+        </article>
+        <?php
+    endwhile;
+    ?>
+</main>
+
+<?php get_footer(); ?>
