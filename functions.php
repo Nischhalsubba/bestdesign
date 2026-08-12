ff3d02e43aa27bf31ccb79f97b6852e5366c36ff
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-12 09:33:39 +0545

    Clean and document WordPress theme bootstrap

--- functions.php
+++ functions.php
@@ -1,207 +1,186 @@
 <?php
-    add_theme_support( 'menus' );
-    add_theme_support( 'post-thumbnails' ); 
+/**
+ * Theme bootstrap for the Best Design WordPress site.
+ * Registers theme features, public assets, menus, custom post types, sidebars,
+ * and the small content helpers used by the legacy templates.
+ */
 
+add_theme_support( 'menus' );
+add_theme_support( 'post-thumbnails' );
 
-function google_fonts(){
-    wp_register_style('oswald','//fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700');
-    wp_enqueue_style('oswald');
-
-    wp_register_style('poppins','//fonts.googleapis.com/css?family=Poppins:100,100i,200,300,400,500,600,700,800');
-    wp_enqueue_style('poppins');
-
-    wp_register_style('material','//fonts.googleapis.com/icon?family=Material+Icons');
-    wp_enqueue_style('material');
+/** Enqueues the fonts and icon family used by the public theme. */
+function bestdesign_enqueue_fonts() {
+    wp_enqueue_style( 'bestdesign-oswald', '//fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700', array(), null );
+    wp_enqueue_style( 'bestdesign-poppins', '//fonts.googleapis.com/css?family=Poppins:100,100i,200,300,400,500,600,700,800', array(), null );
+    wp_enqueue_style( 'bestdesign-material-icons', '//fonts.googleapis.com/icon?family=Material+Icons', array(), null );
 }
-add_action('wp_enqueue_scripts','google_fonts');
+add_action( 'wp_enqueue_scripts', 'bestdesign_enqueue_fonts' );
 
-function enqueue_my_custom_styles() {
-    wp_register_style('owl-theme-default', get_template_directory_uri().'./owl carousel/owl.theme.default.min.css', array(),false, 'all' );
-    wp_enqueue_style('owl-theme-default');
-
-    wp_register_style('owl-carousel-min', get_template_directory_uri().'./owl carousel/owl.carousel.min.css', array(),false, 'all' );
-    wp_enqueue_style('owl-carousel-min');
-
-    wp_register_style('fontawesome', 'https://use.fontawesome.com/releases/v5.7.2/css/all.css', array(),false, 'all' );
-    wp_enqueue_style('fontawesome');
-
-    //wp_register_style('my-own-css', get_template_directory_uri().'./css/style.css', array(),false, 'all' );
-    //wp_enqueue_style('my-own-css');
+/** Enqueues the canonical theme stylesheet and third-party carousel/icon styles. */
+function bestdesign_enqueue_styles() {
+    wp_enqueue_style( 'bestdesign-theme', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+    wp_enqueue_style( 'bestdesign-owl-theme', get_template_directory_uri() . '/owl carousel/owl.theme.default.min.css', array(), null );
+    wp_enqueue_style( 'bestdesign-owl-carousel', get_template_directory_uri() . '/owl carousel/owl.carousel.min.css', array(), null );
+    wp_enqueue_style( 'bestdesign-fontawesome', 'https://use.fontawesome.com/releases/v5.7.2/css/all.css', array(), '5.7.2' );
 }
+add_action( 'wp_enqueue_scripts', 'bestdesign_enqueue_styles' );
 
-add_action( 'wp_enqueue_scripts', 'enqueue_my_custom_styles' );
-
-
-function include_jquery(){
-    wp_deregister_script('jquery');
-
-    wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js', '', 1,true);
-
-    add_action('wp_enqueue_scripts','jquery');
-}
-add_action('wp_enqueue_scripts','include_jquery');
-
-
-function loadjs()
-{
-    wp_register_script('owl-carousel', get_template_directory_uri(). './owl carousel/owl.carousel.min.js', '' ,1, true);
-    wp_enqueue_script('owl-carousel');
-
-    wp_register_script('indexjs', get_template_directory_uri(). './js/index.js', '' ,1, true);
-    wp_enqueue_script('indexjs');
-
-    wp_register_script('parallax', 'https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js', '', 1,true);
-    wp_enqueue_script('parallax');
-
-}
-
-add_action('wp_enqueue_scripts','loadjs');
-
-//Menu
-function register_my_menu() {
-  register_nav_menu('header-menu',__( 'Header Menu' ));
-}
-add_action( 'init', 'register_my_menu' );
-
-function register_my_menus() {
-  register_nav_menus(
-    array(
-      'header-menu' => __( 'Header Menu' ),
-      'footer-menu' => __( 'Footer Menu' )
-    )
-  );
-}
-add_action( 'init', 'register_my_menus' );
-
-function remove_ul ( $menu ){
-  return preg_replace( array( '#^<ul[^>]*>#', '#</ul>$#' ), '', $menu );
-}
-add_filter( 'wp_nav_menu', 'remove_ul' );
-
-
-function create_services_post_type() {
-    register_post_type( 'bestdesign_services',
-      array(
-        'labels' => array(
-          'name' => __( 'Services' ),
-          'singular_name' => __( 'Service' ),
-          'add_new' => 'Add Services', 
-        'add_new_item' => 'Add Services',
-        ),
-        'public' => true,
-        'has_archive' => true,
-        'supports'    => array( 'title', 'editor','thumbnail', 'excerpt'),
-        'taxonomies'  => array( 'category' )
-      )
+/** Enqueues the theme interaction script and its third-party dependencies. */
+function bestdesign_enqueue_scripts() {
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script(
+        'bestdesign-owl-carousel',
+        get_template_directory_uri() . '/owl carousel/owl.carousel.min.js',
+        array( 'jquery' ),
+        null,
+        true
     );
-  }
-
-  add_action( 'init', 'create_services_post_type' );
-
-
-if( function_exists('acf_add_options_page') ) {
-	
-	acf_add_options_page();
+    wp_enqueue_script(
+        'bestdesign-theme',
+        get_template_directory_uri() . '/js/index.js',
+        array( 'jquery', 'bestdesign-owl-carousel' ),
+        wp_get_theme()->get( 'Version' ),
+        true
+    );
+    wp_enqueue_script(
+        'bestdesign-parallax',
+        'https://cdn.jsdelivr.net/parallax.js/1.4.2/parallax.min.js',
+        array( 'jquery' ),
+        '1.4.2',
+        true
+    );
 }
+add_action( 'wp_enqueue_scripts', 'bestdesign_enqueue_scripts' );
 
-
-function create_work_post_type() {
-  register_post_type( 'bestdesign_work',
-    array(
-      'labels' => array(
-        'name' => __( 'works' ),
-        'singular_name' => __( 'work' ),
-        'add_new' => 'Add Work', 
-        'add_new_item' => 'Add Work',
-      ),
-      'public' => true,
-      'has_archive' => true,
-      'supports'    => array( 'title','thumbnail', 'excerpt'),
-      'taxonomies'  => array( 'category' )
-    )
-  );
+/** Registers the header and footer navigation locations exposed by the theme. */
+function bestdesign_register_menus() {
+    register_nav_menus(
+        array(
+            'header-menu' => __( 'Header Menu', 'bestdesign' ),
+            'footer-menu' => __( 'Footer Menu', 'bestdesign' ),
+        )
+    );
 }
+add_action( 'init', 'bestdesign_register_menus' );
 
-add_action( 'init', 'create_work_post_type' );
-
-
-function create_say_post_type() {
-  register_post_type( 'bd_testimonial',
-    array(
-      'labels' => array(
-        'name' => __( 'Testimonials' ),
-        'singular_name' => __( 'Testimonial' ),
-        'add_new' => 'Add Testimonial', 
-        'add_new_item' => 'Add Testimonial',
-      ),
-      'public' => true,
-      'has_archive' => true,
-      'supports'    => array( 'title','thumbnail', 'excerpt','editor'),
-      'taxonomies'  => array( 'category' )
-    )
-  );
+/** Removes WordPress's outer list wrapper where the legacy markup supplies its own list. */
+function bestdesign_remove_menu_list_wrapper( $menu ) {
+    return preg_replace( array( '#^<ul[^>]*>#', '#</ul>$#' ), '', $menu );
 }
+add_filter( 'wp_nav_menu', 'bestdesign_remove_menu_list_wrapper' );
 
-add_action( 'init', 'create_say_post_type' );
-
-
-//Renaming Testimonial
-function wpb_change_title_text( $title ){
-  $screen = get_current_screen();
-
-  if  ( 'bd_testimonial' == $screen->post_type ) {
-       $title = 'Enter the Name here';
-  }
-
-  return $title;
+/** Registers the Services content type used by the home and service templates. */
+function bestdesign_register_services_post_type() {
+    register_post_type(
+        'bestdesign_services',
+        array(
+            'labels' => array(
+                'name'          => __( 'Services', 'bestdesign' ),
+                'singular_name' => __( 'Service', 'bestdesign' ),
+                'add_new'       => __( 'Add Service', 'bestdesign' ),
+                'add_new_item'  => __( 'Add Service', 'bestdesign' ),
+            ),
+            'public'      => true,
+            'has_archive' => true,
+            'supports'    => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+            'taxonomies'  => array( 'category' ),
+        )
+    );
 }
+add_action( 'init', 'bestdesign_register_services_post_type' );
 
-
-
-add_filter( 'enter_title_here', 'wpb_change_title_text' );
-
-//Registering widget
-function arphabet_widgets_init() {
-
-  register_sidebar(
-		array(
-			'id'            => 'sidebar-home',
-			'name'          => esc_html__( 'Our Work', 'bestdesign' ),
-			'before_widget' => '<div id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+/** Adds the ACF options page when Advanced Custom Fields Pro is available. */
+function bestdesign_register_acf_options_page() {
+    if ( function_exists( 'acf_add_options_page' ) ) {
+        acf_add_options_page();
+    }
 }
-add_action( 'widgets_init', 'arphabet_widgets_init' );
+add_action( 'acf/init', 'bestdesign_register_acf_options_page' );
 
-function shortcode_widgets_init() {
-
-  register_sidebar(
-		array(
-			'id'            => 'Shortcode-home',
-			'name'          => esc_html__( 'Shortcode', 'bestdesign' ),
-			'before_widget' => '<div id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</div>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+/** Registers the portfolio/work content type used by project listings. */
+function bestdesign_register_work_post_type() {
+    register_post_type(
+        'bestdesign_work',
+        array(
+            'labels' => array(
+                'name'          => __( 'Works', 'bestdesign' ),
+                'singular_name' => __( 'Work', 'bestdesign' ),
+                'add_new'       => __( 'Add Work', 'bestdesign' ),
+                'add_new_item'  => __( 'Add Work', 'bestdesign' ),
+            ),
+            'public'      => true,
+            'has_archive' => true,
+            'supports'    => array( 'title', 'thumbnail', 'excerpt' ),
+            'taxonomies'  => array( 'category' ),
+        )
+    );
 }
-add_action( 'widgets_init', 'shortcode_widgets_init' );
+add_action( 'init', 'bestdesign_register_work_post_type' );
 
-
-
-//Setting words limit in excerpt
-function word_count($string, $limit) {
- 
-  $words = explode(' ', $string);
-   
-  return implode(' ', array_slice($words, 0, $limit));
+/** Registers the testimonial content type displayed by the home-page carousel. */
+function bestdesign_register_testimonial_post_type() {
+    register_post_type(
+        'bd_testimonial',
+        array(
+            'labels' => array(
+                'name'          => __( 'Testimonials', 'bestdesign' ),
+                'singular_name' => __( 'Testimonial', 'bestdesign' ),
+                'add_new'       => __( 'Add Testimonial', 'bestdesign' ),
+                'add_new_item'  => __( 'Add Testimonial', 'bestdesign' ),
+            ),
+            'public'      => true,
+            'has_archive' => true,
+            'supports'    => array( 'title', 'thumbnail', 'excerpt', 'editor' ),
+            'taxonomies'  => array( 'category' ),
+        )
+    );
 }
+add_action( 'init', 'bestdesign_register_testimonial_post_type' );
 
+/** Rewords the testimonial title placeholder so editors know to enter a client name. */
+function bestdesign_testimonial_title_placeholder( $title ) {
+    $screen = get_current_screen();
 
+    if ( $screen && 'bd_testimonial' === $screen->post_type ) {
+        return __( 'Enter the name here', 'bestdesign' );
+    }
 
-?>
+    return $title;
+}
+add_filter( 'enter_title_here', 'bestdesign_testimonial_title_placeholder' );
 
+/** Registers the home-page Our Work widget area. */
+function bestdesign_register_work_sidebar() {
+    register_sidebar(
+        array(
+            'id'            => 'sidebar-home',
+            'name'          => esc_html__( 'Our Work', 'bestdesign' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+}
+add_action( 'widgets_init', 'bestdesign_register_work_sidebar' );
+
+/** Registers the legacy Shortcode widget area used by existing page content. */
+function bestdesign_register_shortcode_sidebar() {
+    register_sidebar(
+        array(
+            'id'            => 'shortcode-home',
+            'name'          => esc_html__( 'Shortcode', 'bestdesign' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
+}
+add_action( 'widgets_init', 'bestdesign_register_shortcode_sidebar' );
+
+/** Returns the first requested number of whitespace-separated words from a string. */
+function bestdesign_limit_words( $string, $limit ) {
+    $words = preg_split( '/\s+/', trim( $string ) );
+    return implode( ' ', array_slice( $words, 0, max( 0, (int) $limit ) ) );
+}
